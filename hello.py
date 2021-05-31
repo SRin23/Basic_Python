283817e70f77cd283cedf3f4042c88505e9b2c77
SRin23 <serin4723@naver.com>
2021-05-31 13:15:22 +0900

    first commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+#print()는 함수
+#()소괄호는 어떤걸 print할지 알려줌
+#사람이 할수있는 일과 python이 할수 있는 일을 나눔
+#반드시 따옴표의 짝이 맞아야한다.
+#powershell에서 무한루프를 만나면 exit()하면 빠져나옴
+
+print('Hello, world')
+print('Hello, world')
+print('Hello, world')
