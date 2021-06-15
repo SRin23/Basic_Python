41048b6ebe9cdbba5be7c163a591e4b67e9a494b
김세린 <68180798+SRin23@users.noreply.github.com>
2021-06-15 10:39:42 +0000

    6/15 commit

---
+++ dic_change.py
@@ -0,0 +1,19 @@
+#딕셔너리 수정하기
+
+#리스트 수정
+list = [1, 2, 3, 4, 5]
+list[2] = 33
+#리스트 값 추가
+list.append(6)
+del(list[0])
+#딕셔너리 생성
+dict = {'one' : 1, 'two' : 2}
+
+#딕셔너리 수정
+dict['one'] = 11
+
+#딕셔너리 추가
+dict['three'] =  3
+del(dict['one'])
+
+#딕셔너리 삭제
